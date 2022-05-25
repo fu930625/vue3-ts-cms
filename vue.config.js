7dde812b5a825798987ccd464b036cf0d25a5f78
futing <futt9468@fxiaoke.com>
2022-05-25 16:03:54 +0800

    配置webpack的配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,42 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
+const path = require('path')
+
+function resolve(dir)  {
+  return path.join(__dirname,dir)
+}
+module.exports = {
+  //方式一:CLI提供的属性
+  outputDir: "./build",
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http: //152.136.185.210:4000',
+        pathRewrite: {
+          '^/api': ''
+        },
+        changeOrigin: true
+      }
+    }
+  },
+  //2: 和webpack属性完全一致,最后会进行合并
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // }
+  //3:
+  chainWebpack: (config) => {
+    config.resolve.alias
+    // .set('@',path.resolve(__dirname,'src'))
+    // .set('components', '@/components')
+    .set('@', resolve('src'))
+    .set('components', resolve('@/components'))
+
+  }
+}

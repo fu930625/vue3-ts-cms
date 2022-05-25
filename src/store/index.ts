dc9d7a307efabb29a81d2ff981b219eca6c2209d
futing <futt9468@fxiaoke.com>
2022-05-25 17:22:12 +0800

    配置vuex

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      name: 'melting'
+    }
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+})
+export  default store

7119813ff2191b3b6db0f3063eca1937c626487b
futing <futt9468@fxiaoke.com>
2022-06-04 17:42:15 +0800

    安装css 基础样式

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import router from "./router"
 import store from "./store"
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
-
-
+import './assets/css/index.less'
+import 'normalize.css'
 const app = createApp(App)
 
 app.use(router)
